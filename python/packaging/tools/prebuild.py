5b8b1b5f4943c9aec15f5722e9f5171d963d9062
frommwonderland <daniel.illner@outlook.de>
2022-06-23 22:44:24 +0200

    prebuild updates python packaging

--- python/packaging/tools/prebuild.py
+++ python/packaging/tools/prebuild.py
@@ -11,3 +11,25 @@ def build_poco(cmake: CMake = None) -> None:
 
     cmake.generate()
     cmake.build(my_env)
+
+
+def build_pybind(cmake: CMake = None) -> None:
+    """."""
+    my_env = os.environ.copy()
+
+    if cmake is None:
+        cmake = CMake("ThirdParty/pybind11/build")
+
+    cmake.generate()
+    cmake.build(my_env)
+
+
+def build_iou(cmake: CMake = None) -> None:
+    """."""
+    my_env = os.environ.copy()
+
+    if cmake is None:
+        cmake = CMake("ThirdParty/IoU/release")
+
+    cmake.generate()
+    cmake.build(my_env)
